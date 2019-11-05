91ab19fc1222a11616616d98b89d31b56fa24f32
Art Raj Tiwari <artrajtiwari@gmail.com>
2019-11-05 22:53:49 +0545

    make use of posted form data

--- pizza/views.py
+++ pizza/views.py
@@ -7,6 +7,17 @@ def home(request):
 
 
 def order(request):
-    form = PizzaForm()
-    context = {'pizzaform': form}
-    return render(request, 'order.html', context)
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Your %s %s and %s pizza is on its way! Thank you for visiting us.' % (
+                filled_form.cleaned_data['size'], filled_form.cleaned_data['topping1'],
+                filled_form.cleaned_data['topping2'])
+            new_form = PizzaForm()
+            context = {'pizzaform': new_form,
+                       'note': note}
+            return render(request, 'order.html', context)
+    else:
+        form = PizzaForm()
+        context = {'pizzaform': form}
+        return render(request, 'order.html', context)
