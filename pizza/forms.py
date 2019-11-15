23466754d6efb6bf3334fed96986911a0c3021b4
Art Raj Tiwari <artrajtiwari@gmail.com>
2019-11-15 16:14:41 +0545

    add changes to models ,forms and admi.py

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from .models import Pizza
 
 
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label="Topping One: ", max_length=100)
-    topping2 = forms.CharField(label="Topping Two: ", max_length=100)
-    size = forms.ChoiceField(label="Sizes:",choices=[('small','small'),('medium','medium'),('large','large')])
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label="Topping One: ", max_length=100)
+#     topping2 = forms.CharField(label="Topping Two: ", max_length=100)
+#     size = forms.ChoiceField(label="Sizes:",choices=[('small','small'),('medium','medium'),('large','large')])
+
+
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['toppings1', 'toppings2', 'size']
--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Pizza(models.Model):
+    toppings1 = models.CharField(max_length=100)
+    toppings2 = models.CharField(max_length=100)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
